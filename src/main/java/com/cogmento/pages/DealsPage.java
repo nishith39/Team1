3f0b1571217dfcb5cb5884c5665d7b50d2de3e4c
sathvikt28 <sathvikt28@gmail.com>
2024-05-27 11:46:35 +0530

    Functions are added - Create and Delete.. Hashmap using excel

--- src/main/java/com/cogmento/pages/DealsPage.java
+++ src/main/java/com/cogmento/pages/DealsPage.java
@@ -1,7 +1,10 @@
 package com.cogmento.pages;
 
+import com.cogmento.utils.CustomException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
 /*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;*/
 
@@ -13,14 +16,19 @@ public class DealsPage extends BasePage{
 
     private final By titleDeal = By.xpath("//label[text()='Title']//following-sibling::div//input");
     private final By descriptionDeal = By.xpath("//label[text()='Description']//following-sibling::textarea");
+    private final By errorToast = By.xpath("//div[@class=\"content\"]//p");
 
-    public void createDeal(String title, String description,boolean check) throws Exception {
+    public void createDeal(HashMap<String,String> data,boolean check) throws Exception {
         clickOnCreate();
-        enterDealsDetails(title, description);
+        enterDealsDetails(data);
         clickOnSave();
         if(check) {
-            checkDealsHeader(title);
+            checkDealsHeader(data.get("Title"));
         }
+    }
+
+    public void editDeal(String title, HashMap<String,String> data) throws Exception {
+        clickOnEdit(title,data);
     }
     public void deleteDeal(String title,String popupValue,boolean look) throws Exception {
         deleteRecord(title,popupValue,look);
@@ -30,12 +38,15 @@ public class DealsPage extends BasePage{
     public void clickOnCreate () throws Exception {
         createButton();
     }
-    /*public void performActions() throws Exception {
-        performTableOperation("Google","view");
-    }*/
-    public void enterDealsDetails (String title, String description){
-        scriptAction.inputText(titleDeal, title);
-        scriptAction.inputText(descriptionDeal, description);
+    public void enterDealsDetails (HashMap<String,String> data){
+        for (String s : data.keySet()){
+            if (s.equalsIgnoreCase("Title")){
+                scriptAction.inputText(titleDeal,data.get(s));
+            }
+            if(s.equalsIgnoreCase("Description")){
+                scriptAction.inputText(descriptionDeal,data.get(s));
+            }
+        }
     }
 
     public void clickOnSave () throws Exception {
@@ -46,8 +57,17 @@ public class DealsPage extends BasePage{
         checkPageHeader(title);
     }
 
-    public void clickOnDelete(String title, String popUpValue,boolean look) throws Exception {
-        deleteRecord(title, popUpValue,look);
+    public void clickOnEdit(String title,HashMap<String,String> data) throws Exception {
+        performTableOperation(title,"edit");
+
+//        scriptAction.clearAndInputText(titleDeal,editTitle);
+//        scriptAction.clearAndInputText(descriptionDeal,editDescription);
+        enterDealsDetails(data);
     }
+
+    public void maxCharacter(){
+
+    }
+
 
 }
--- src/test/java/com/cogmento/testcases/cases/CreateDeal.java
+++ src/test/java/com/cogmento/testcases/cases/CreateDeal.java
@@ -4,11 +4,17 @@ import com.cogmento.pages.*;
 import com.cogmento.reporting.ExtentTestManager;
 import com.cogmento.testcases.BaseTest;
 import com.cogmento.utils.CustomException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testcontainers.shaded.org.apache.commons.io.filefilter.TrueFileFilter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class CreateDeal extends BaseTest {
@@ -20,6 +26,31 @@ public class CreateDeal extends BaseTest {
     private DealsPage dealsPage;
     private String userEmailId, userPwd;
     private HashMap<String, String> testData = new HashMap<>();
+
+    public HashMap createDealData() throws IOException {
+        HashMap<String, String> data = new HashMap<>();
+        File filename = new File("C:\\Users\\Sathvik\\Downloads\\deals.xlsx");
+        FileInputStream finput = new FileInputStream(filename);
+        XSSFWorkbook workbook = new XSSFWorkbook(finput);
+        XSSFSheet sheet = workbook.getSheet("DealsSheet");
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            String key = sheet.getRow(i).getCell(0).getStringCellValue();
+            String value = sheet.getRow(i).getCell(1).getStringCellValue();
+            data.put(key,value);
+        }
+        return data;
+    }
+//        data.put("Title","");
+//        data.put("Description","Samsung retook the Deal");
+//        return data;
+//    }
+//
+//    public HashMap editDealData(){
+//        HashMap<String,String> data = new HashMap<>();
+//        data.put("Title","Facebook");
+//        data.put("Description","Facebook back in the race");
+//        return data;
+//    }
 
     @BeforeMethod
     public void setUp() {
@@ -37,8 +68,8 @@ public class CreateDeal extends BaseTest {
         //casesPage = new CasesPage(getDriver());
     }
 
-    @Test(description = "Create Deals")
-    public void testCaseFlow() throws Exception {
+    @Test(priority = 1, description = "Create Deals")
+    public void testCaseFlow1() throws Exception {
         try {
 
             // Login Page - Login to Application
@@ -47,11 +78,63 @@ public class CreateDeal extends BaseTest {
 
             // Step 1 :  Create Company
             homePage.selectEntity(EntityPanel.Deals);
-            //casesPage.createCase(testData).verifyCase(testData.get("Title"));
-            dealsPage.createDeal("Google", "Google Deal Done",true);
+            HashMap<String,String> data = createDealData();
+            dealsPage.createDeal(data,true);
+//            dealsPage.createDeal("GoogleDoodle", "Google Deal Done",true);
         } catch (Exception e) {
             throw new CustomException(e);
 
+        }
+    }
+
+    @Test(priority = 2, description = "Edit Deals")
+    public void testCaseFlow2() throws Exception {
+        try {
+
+            // Login Page - Login to Application
+            loginPage.loginToApplication(userEmailId, userPwd);
+            ExtentTestManager.getTest().pass("Logged in to application");
+
+            // Step 1 :  Create Company
+            homePage.selectEntity(EntityPanel.Deals);
+            HashMap<String,String> data = createDealData();
+            dealsPage.editDeal("Samsung",data);
+//            dealsPage.clickOnSave();
+//            dealsPage.editDeal("GoogleDoodle","Apple","Apple Took Over the deal !!");
+        } catch (Exception e) {
+            throw new CustomException(e);
+
+        }
+    }
+
+    @Test(priority = 3, description = "No Mandatory")
+    public void testcaseFlow3() throws Exception {
+        try {
+            loginPage.loginToApplication(userEmailId,userPwd);
+            ExtentTestManager.getTest().pass("Logged into Application");
+
+            homePage.selectEntity(EntityPanel.Deals);
+            HashMap<String,String> data = createDealData();
+            dealsPage.createDeal(data,false);
+            dealsPage.verifyErrorMessage("The field Title is required.");
+        } catch (Exception e) {
+            throw new CustomException(e);
+        }
+    }
+
+    @Test(priority = 4, description = "Delete Deals")
+    public void testCaseFlow4() throws Exception {
+        try {
+
+            /* Login Page - Login to Application */
+            loginPage.loginToApplication(userEmailId, userPwd);
+            ExtentTestManager.getTest().pass("Logged in to application");
+
+            // Step 1 :  Create Company
+            homePage.selectEntity(EntityPanel.Deals);
+            dealsPage.deleteDeal("GoogleDoodle","Delete",true);
+        } catch (Exception e) {
+            throw new CustomException(e);
         }
     }
 
